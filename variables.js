c13cd460912e77fcb20f15bc0193c58788c93891
Alice Abarca <aliceabarca@otmail.com>
2023-02-28 14:26:56 -0700

    Add the reassigned values and call the variables

--- variables.js
+++ variables.js
@@ -1,11 +1,27 @@
 var dogName = "Rocco"
-var birdName = "Jules"
-var catName = "Thomas"
+dogName = "Tim"
+console.log(dogName);
 
 var dogAge = 3
-var birdAge = 1
-var catAge = 5
-
 var dogAdopted = true
+console.log(dogAge);
+console.log(dogAdopted);
+
+
+var birdName = "Jules"
+birdName = "Birdy"
+console.log(birdName);
+
+var birdAge = 1
 var birdAdopted = false
+console.log(birdAge);
+console.log(birdAdopted);
+
+var catName = "Thomas"
+catName = "Kitty"
+console.log(catName);
+
+var catAge = 5
 var catAdopted = true
+console.log(catAge);
+console.log(catAdopted);
